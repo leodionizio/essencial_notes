754a64c66b48048ae98b039682414795ac37a39d
Leonardo Dionizio <leodionizio13@gmail.com>
2017-11-13 08:50:20 -0200

    att items home

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -27,12 +27,12 @@ export class HomePage {
 
   private getNotes(): void {
     // this.notes = [
-    //   { id: 1, title: 'Alguma coisa', color: 'dark', content: 'aniotação teste', date_created: '', date_edited: '' },
-    //   { id: 1, title: 'Alguma coisa', color: 'primary', content: 'aniotação teste', date_created: '', date_edited: '' },
-    //   { id: 1, title: 'Alguma coisa', color: 'secondary', content: 'aniotação teste', date_created: '', date_edited: '' },
-    //   { id: 1, title: 'Alguma coisa', color: 'warning', content: 'aniotação teste', date_created: '', date_edited: '' },
-    //   { id: 1, title: 'Alguma coisa', color: 'dark', content: 'aniotação teste', date_created: '', date_edited: '' },
-    //   { id: 1, title: 'Alguma coisa', color: 'purple', content: 'aniotação teste', date_created: '', date_edited: '' },
+    //   { id: 1, key: 'A', title: 'Alguma coisa', color: 'dark', content: 'aniotação teste', date_created: '', date_edited: '' },
+    //   { id: 1, key: 'A', title: 'Alguma coisa', color: 'primary', content: 'aniotação teste', date_created: '', date_edited: '' },
+    //   { id: 1, key: 'A', title: 'Alguma coisa', color: 'secondary', content: 'aniotação teste', date_created: '', date_edited: '' },
+    //   { id: 1, key: 'A', title: 'Alguma coisa', color: 'warning', content: 'aniotação teste', date_created: '', date_edited: '' },
+    //   { id: 1, key: 'A', title: 'Alguma coisa', color: 'dark', content: 'aniotação teste', date_created: '', date_edited: '' },
+    //   { id: 1, key: 'A', title: 'Alguma coisa', color: 'purple', content: 'aniotação teste', date_created: '', date_edited: '' },
     // ]
     this.notesService.getAll()
       .then((notes: NotesModel[]) => {
